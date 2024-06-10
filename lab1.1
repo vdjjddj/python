b936193480085b4585acc861bf55620c1d3f2792
ZaneL777 <6629011012@cdti.ac.th>
2024-06-10 16:36:50 +0700

    first commit

---
+++ lab1.1
@@ -0,0 +1,5 @@
+print("welcome to นรก")
+x = 10
+y = 20
+z = x + y
+print("คำตอบ\n"+str(z)+"ครับ")
---
+++ lab1.3.py
@@ -0,0 +1,4 @@
+print("ยินดีต้อนรับ")
+r = int(input("ความยาวรัศมีวงกลม:"))
+area = 22/7*(r*r)
+print("Circle area: " + str(area))
