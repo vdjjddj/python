63e21e762bab8ad434d20f6995c62f90efa07352
ZaneL777 <6629011012@cdti.ac.th>
2024-06-17 16:10:30 +0700

    update

---
+++ lab3.3.py
@@ -0,0 +1,7 @@
+x=int(input("แม่สูตรคูณ:"))
+y=1
+while y<=12:
+    z=x*y
+    y+=1
+    print("%d x %d = %d" % (x,y,z))
+    y+=1
---
+++ lab3.5.py
@@ -0,0 +1,3 @@
+yaer = [2020, 2022, 2023, 2024]
+for n in yaer
+    print(n)
